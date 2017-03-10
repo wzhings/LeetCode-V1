b40e5fcad3a5984c9621270634f160c3750c2495
Comma Wang <wzhings@gmail.com>
2017-03-09 21:21:45 -0500

    Add files via upload

---
+++ 16-3SumCloset.cpp
@@ -0,0 +1,31 @@
+// No. 16 - 3 Sum Closet
+
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int result = 0;
+        int min_gap = INT_MAX;
+        sort(nums.begin(), nums.end());
+        for(auto a = nums.begin(); a != prev(nums.end(), 2); ++a)  // notice fuc: prev()
+        {
+	        auto b = next(a);
+	        auto c = prev(nums.end());
+	        while(b < c)
+	        {
+		        const int sum = *a + *b + *c;
+		        const int gap = abs(sum - target);
+		        if (gap < min_gap)
+		        {
+			        result = sum;
+			        min_gap = gap;
+		        }
+		        if (sum < target)  // This part is important!
+			        ++b;
+		        else
+			        --c;
+	        }
+        }
+    return result;
+    }
+};
